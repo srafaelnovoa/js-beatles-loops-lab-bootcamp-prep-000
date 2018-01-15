4e7cbfc0f62e67aef5f3bba1b9ffbf3587d82290
SEGUNDO R NOVOA <rafael.novoa@gmail.com>
2018-01-15 16:20:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(arrayMusicians,arrayInstruments) {
   var array = {};
   for(var i = 0;i < arrayMusicians.length;i++) {
     //console.log(arrayMusicians[i])
-    array[i] = "${arrayMusicians[i]} plays ${arrayInstruments[i]}"
+    array[i] = arrayMusicians[i] +" plays " + arrayInstruments[i]
     console.log(array[i])
   }
 
